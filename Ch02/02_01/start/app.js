c4062aed9acbe8891efd4d1cebce13235c763894
David Zheng <zhengdav@msu.edu>
2020-01-30 22:09:37 -0800

    wrote JSX, element shows up on live server, but not on local web server

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -2,23 +2,21 @@
   "use strict";
 
   function ProductImage(props) {
-    return React.createElement('img', {
-      src: '../../../assets/red.jpg', 
-      alt: 'product-image'
-    })
+    return <img src='../../../assets/red.jpg' alt='product-image' />
   }
+  
 
   function ProductCustomizer(props) {
-    return React.createElement(
-      'div',
-      { className: 'customizer' },
-      React.createElement('div', { className: 'product-image'}, React.createElement(ProductImage))
-    )
+    return (
+      <div className='customizer'>
+        <div className='product-image'>
+          <ProductImage />
+        </div>
+      </div>
+    ); 
   }
 
-
   ReactDOM.render(
-    React.createElement(ProductCustomizer), 
-    document.getElementById('react-root')
+    <ProductCustomizer />, document.getElementById('react-root')
   );
 })();
