e92a5a82aa41879a0ebfdf6ad38a94f5f47b9a19
David Zheng <zhengdav@msu.edu>
2020-01-31 14:03:59 -0800

    added size selector

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -1,5 +1,26 @@
-// (function() {
+(function() {
   "use strict";
+
+  function SizeSelector(props) {
+    function sizeOptions() {
+      let sizes = window.Inventory.allSizes;
+      return sizes.map(function(num) {
+        return (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ) 
+      })
+    }
+    return (
+      <div className="field-group">
+        <label htmlFor="size-options">Available Size:</label>
+        <select className="sizeOptions" id="size-options">
+          { sizeOptions() }
+        </select>
+      </div>
+    )
+  }
   
   function ProductImage(props) {
     return <img src='../../../assets/red.jpg' alt='product-image' />
@@ -12,6 +33,9 @@
         <div className='product-image'>
           <ProductImage />
         </div>
+        <div className="selectors">	
+          <SizeSelector />
+        </div>	
       </div>
     ); 
   }
@@ -19,4 +43,4 @@
   ReactDOM.render(
     <ProductCustomizer />, document.getElementById('react-root')
   );
-// })();
+})();
